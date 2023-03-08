4684ee17c72f26478f3374a494a19d02351aef29
Gustavo Sotres <gus.sotres@gmail.com>
2023-03-08 16:16:11 -0600

    Integrated Mongo DB

--- MongoDB.py
+++ MongoDB.py
@@ -1,23 +1,44 @@
+"""Connection to MongoDB"""
+
 from pymongo import MongoClient
-from unittest import result
 
-from activity_SOLID.producto import Producto
+from database import Database
+from producto import Producto
 
 
-class MongoDB:
-    def __init__(self) -> None:
-        self.client = MongoClient("mongodb+srv://admin:root@cluster0.muthfez.mongodb.net/?retryWrites=true&w=majority")
+class MongoDB(Database):
+    """Connection to MongoDB"""
+
+    def insetar(self, producto:Producto) -> None:
+        """Insert in mongoDB"""
+        self.collection.insert_one(
+            {
+                "id": producto.id,
+                "name": producto.nombre,
+                "precio": producto.precio,
+                "cantidad": producto.cantidad
+            }
+        )
+
+    def actulizar(self, id:int, producto: Producto) -> None:
+        """Actualizar en mongo DB"""
+        self.collection.update_one({"id":id}, {
+                "id": producto.id,
+                "name": producto.nombre,
+                "precio": producto.precio,
+                "cantidad": producto.cantidad
+            })
+
+    def eliminar(self, id: int) -> None:
+        """Eliminar en mongoDB"""
+        self.collection.delete_one({"id" : id})
+
+    def abrir(self) -> None:
+        self.client = MongoClient(
+            "mongodb+srv://admin:root@cluster0.muthfez.mongodb.net/?retryWrites=true&w=majority"
+        )
         self.db = self.client["Solid"]
         self.collection = self.db["Productos"]
 
-    def insetar(self, producto:Producto) -> None:
-        self.collection.insert_one({"id": producto.id, "name": producto.nombre, "precio": producto.precio, "cantidad": producto.cantidad})
-
-    def actulizar(self, producto:Producto) -> None:
-        result = self.collection.find(producto.id)
-        self.collection.update_one(result)
-
-    def delete(self, producto:Producto) -> None:
-        result = self.collection.find(producto.id)
-        self.collection.delete_one(result)
-
+    def cerrar(self) -> None:
+        self.client.close()
