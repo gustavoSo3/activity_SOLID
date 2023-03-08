9b9d461483674ac29e9db11ef395b2b457ac682e
Gustavo Sotres <gus.sotres@gmail.com>
2023-03-08 15:34:03 -0600

    Format and following PEP8 Guidelines

--- database.py
+++ database.py
@@ -1,17 +1,15 @@
+"""Database Interface"""
+
 from abc import ABC
 from producto import Producto
 
 class Database(ABC):
     '''Interface para las bases de datos'''
-    def insertar(self,nombre:str, precio:float, cantidad:int) -> None:
+    def insertar(self, producto : Producto) -> None:
         '''Método para insertar en la base de datos'''
-        pass
 
-    def actualizar(self, id:str, producto:Producto) -> None:
+    def actualizar(self, id : str, producto : Producto) -> None:
         '''Método para actualizar en la base de datos'''
-        pass
 
-    def eliminar(self, id:str) -> None:
+    def eliminar(self, id : str) -> None:
         '''Método para eliminar en la base de datos'''
-        pass
-
